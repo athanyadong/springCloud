1c29518bc073cf53f5bc54fdec2f716987ca804b
athan <dongdong980327@163.com>
2022-03-12 12:37:20 +0800

    测试git

---
+++ order-service/src/main/java/cn/itcast/order/mapper/Hello.java
@@ -0,0 +1,16 @@
+package cn.itcast.order.mapper;
+
+/**
+ * hello
+ *
+ * @author athan dongdong980327@163.copm
+ * @version 2022/3/12 12:34
+ * @since jdk11
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("爱你");
+    }
+
+}
